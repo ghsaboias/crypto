a11c6c464c1f56a523f6286936ec93b4204a73a3
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-28 13:20:27 -0300

    Adds top 100

--- appFetch.js
+++ appFetch.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
-
-const url = 'https://api.coingecko.com/api/v3'
+const BASE_URL = 'https://api.coingecko.com/api/v3'
+const oneBillion = 1000000000;
+const coinsSection = document.querySelector('.coins-section')
 
 // Ping
 async function getEndpoint(url, endpoint) {
@@ -17,20 +17,33 @@ async function getEndpoint(url, endpoint) {
 // getEndpoint(url, '/ping')
 
 // Get coins IDs
-async function getIDs() {
-  const coinsArray = await getEndpoint(url, '/coins/list');
-  const retArr = coinsArray.map((coin) => {
-    const retObj = {
-      id: coin.id,
+async function getCoinsInfo(url, endpoint) {
+  const response = await getEndpoint(url, endpoint);
+  const coinsArray = response.map((coin, index) => {
+    return {
+      index: index + 1,
       symbol: coin.symbol,
-      name: coin.name,
+      market_cap: coin.market_cap,
     }
-    return retObj
   })
-  const filteredArr = retArr.filter((coin) => coin.name.length <= 10)
-  filteredArr.sort((coinA, coinB) => coinA.id - coinB.id)
-  console.log(filteredArr)
-  return retArr;
+  console.log(coinsArray)
+  coinsArray.filter((coin) => coin.market_cap >= oneBillion)
+  return coinsArray;
 }
 
-getIDs()
+// Render coins
+function renderCoin({ index, symbol, market_cap }) {
+  const newCoin = document.createElement('p');
+  newCoin.innerText = `${index}. ${symbol.toUpperCase()}: $${market_cap.toLocaleString()} USD`;
+  newCoin.className = 'coin-item';
+  coinsSection.appendChild(newCoin);
+}
+
+function renderCoins (array) {
+  array.forEach((coin) => renderCoin(coin));
+}
+
+window.onload = async () => {
+  const coinsObj = await getCoinsInfo(BASE_URL, '/coins/markets?vs_currency=usd');
+  renderCoins(coinsObj);
+}
