5c140371f3bc06fbae56bc1dc9f89fcd7f150ce8
Guilherme Saboia <gsaboia@yahoo.com>
2021-10-21 19:25:56 -0300

    Adds contractExplorer

--- web3/app.js
+++ web3/app.js
@@ -1,18 +1,25 @@
-const Web3 = require('web3')
+const Web3 = require("web3");
 
-const url = 'https://mainnet.infura.io/v3/a0fec1f1ddb449768d0646ea800e0b4c';
+const url = "https://mainnet.infura.io/v3/a0fec1f1ddb449768d0646ea800e0b4c";
 
 const web3 = new Web3(url);
-const address = '0x13e965baafda80c7501c23cbed282d5e53e3066b';
+const MY_ADDRESS = "0x13e965baafda80c7501c23cbed282d5e53e3066b";
 
-// web3.eth.getBalance(address).then((result) => { const balanceEth = web3.utils.fromWei(result) });
+// let balanceEth;
 
-async function obtainBalance(address) {
-  const balanceWei =  await web3.eth.getBalance(address);
-  const balanceEth = web3.utils.fromWei(balanceWei);
-  return balanceEth;
-}
+// balance is stored in balanceEth global variable
+// web3.eth.getBalance(MY_ADDRESS).then((result) => { balanceEth = web3.utils.fromWei(result) });
 
-// obtainBalance(address)
+// setTimeout(() => console.log(balanceEth), 3000)
 
-web3.eth.getGasPrice().then((priceWei) => { const price = web3.utils.fromWei(priceWei, 'gwei'); console.log(price); return price })
+// async function fetchBalance(MY_ADDRESS) {
+//   const balanceWei =  await web3.eth.getBalance(MY_ADDRESS);
+//   const balanceEth = web3.utils.fromWei(balanceWei);
+//   console.log(balanceEth);
+//   return balanceEth;
+// }
+
+// const balance = fetchBalance(MY_ADDRESS);
+
+// Get current gas price
+// web3.eth.getGasPrice().then((priceWei) => { const price = web3.utils.fromWei(priceWei, 'gwei'); console.log('Current gas price:', price)})
---
+++ web3/contractExplorer.js
@@ -0,0 +1,246 @@
+const Web3 = require("web3");
+
+const url = "https://mainnet.infura.io/v3/a0fec1f1ddb449768d0646ea800e0b4c";
+
+const web3 = new Web3(url);
+const MY_ADDRESS = "0x13e965baafda80c7501c23cbed282d5e53e3066b";
+const LDO_CONTRACT_ADDRESS = "0x5a98fcbea516cf06857215779fd812ca3bef1b32";
+const RGT_CONTRACT_ADDRESS = "0xD291E7a03283640FDc51b121aC401383A46cC623";
+
+const ABI = [
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+          {
+              "name": "",
+              "type": "string"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_spender",
+              "type": "address"
+          },
+          {
+              "name": "_value",
+              "type": "uint256"
+          }
+      ],
+      "name": "approve",
+      "outputs": [
+          {
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_from",
+              "type": "address"
+          },
+          {
+              "name": "_to",
+              "type": "address"
+          },
+          {
+              "name": "_value",
+              "type": "uint256"
+          }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+          {
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint8"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [
+          {
+              "name": "_owner",
+              "type": "address"
+          }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+          {
+              "name": "balance",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+          {
+              "name": "",
+              "type": "string"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_to",
+              "type": "address"
+          },
+          {
+              "name": "_value",
+              "type": "uint256"
+          }
+      ],
+      "name": "transfer",
+      "outputs": [
+          {
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [
+          {
+              "name": "_owner",
+              "type": "address"
+          },
+          {
+              "name": "_spender",
+              "type": "address"
+          }
+      ],
+      "name": "allowance",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "fallback"
+  },
+  {
+      "anonymous": false,
+      "inputs": [
+          {
+              "indexed": true,
+              "name": "owner",
+              "type": "address"
+          },
+          {
+              "indexed": true,
+              "name": "spender",
+              "type": "address"
+          },
+          {
+              "indexed": false,
+              "name": "value",
+              "type": "uint256"
+          }
+      ],
+      "name": "Approval",
+      "type": "event"
+  },
+  {
+      "anonymous": false,
+      "inputs": [
+          {
+              "indexed": true,
+              "name": "from",
+              "type": "address"
+          },
+          {
+              "indexed": true,
+              "name": "to",
+              "type": "address"
+          },
+          {
+              "indexed": false,
+              "name": "value",
+              "type": "uint256"
+          }
+      ],
+      "name": "Transfer",
+      "type": "event"
+  }
+]
+
+const getMyBalance = async (contractAddress) => {
+  const contractObject = new web3.eth.Contract(ABI, contractAddress);
+  const { methods } = contractObject;
+  const { name } = methods;
+  let tokenName;
+  let myBalance;
+  await name().call().then((result, err) => { tokenName = result });
+  await methods.balanceOf(MY_ADDRESS).call().then((result, err) => { myBalance = web3.utils.fromWei(result) });
+  console.log(`My balance of ${tokenName} is: ${myBalance}`);
+  return [tokenName, myBalance];
+}
+
+getMyBalance(LDO_CONTRACT_ADDRESS);
+getMyBalance(RGT_CONTRACT_ADDRESS);
