0e4c7947653b41c933eb2b3cdba788d13e9e1261
Guilherme Saboia <gsaboia@yahoo.com>
2022-01-06 15:50:58 -0300

    feat: adds getUsdEthPrice function

--- dopex/app.js
+++ dopex/app.js
@@ -1,13 +1,17 @@
 import fetch from 'node-fetch';
 
 // returns DPX price as object with usd and eth keys
-async function getPrice() {
+async function getPriceObj() {
   const ENDPOINT = 'https://api.dopex.io/api/v1/dpx/price';
   const response = await fetch(ENDPOINT);
   const responseJSON = await response.json();
   const { price } = responseJSON;
-  console.log(price);
   return price;
 }
 
-getPrice();
+async function getUsdEthPrice() {
+  const { usd, eth } = await getPriceObj();
+  return { usd, eth };
+}
+
+getUsdEthPrice();
