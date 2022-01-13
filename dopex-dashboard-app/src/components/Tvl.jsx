e7195938ebde8d8e77a587c5bc69eef9cf0c7a4e
Guilherme Saboia <gsaboia@yahoo.com>
2022-01-13 15:13:45 -0300

    refact: improves logic

--- dopex-dashboard-app/src/components/Tvl.jsx
+++ dopex-dashboard-app/src/components/Tvl.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 function Tvl({ tvl, type }) {
-  const title = type === 'farms' ? 'Farms' : 'SSOV';
+  const titleObj = {
+    total: '',
+    farms: 'Farms',
+    ssov: 'SSOV',
+  }
 
   const formattedTvl = (Math.round((tvl + Number.EPSILON) * 100) / 100).toLocaleString();
+
   return (
     <div className="tvl-container">
-      <span>
-        <h3>{ title }</h3>
-        <b>TVL</b> {tvl ? `$${formattedTvl}` : 'Loading...'}
-      </span>
+      <h3>{ titleObj[type] }</h3>
+      <b>TVL</b> {tvl ? `$${formattedTvl}` : 'Loading...'}
     </div>
   )
 }
